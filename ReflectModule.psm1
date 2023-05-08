d20787b4c7376d797bea7baa23d57bd5de9c6145
Cheffromspace <jonflatt@DESKTOP-6M0SR6A>
2023-05-07 19:06:37 -0500

    debug

--- ReflectModule.psm1
+++ ReflectModule.psm1
@@ -11,16 +11,40 @@ function Add-QuestionAndAnswer {
 function Invoke-ReflectGPT {
     param(
         [Parameter(Mandatory=$true)]
-        [string]$Question
+        [string]$Question,
+        [integer]$iterations
     )
+
+    $optimizedQuestion = Add-QuestionAndAnswer -Question $Question
+
     $Responses = @()
 
     # Ask GPT-4 the query three times
     foreach ($i in 0..2) {
         Reset-Conversation
-        $response = Invoke-Conversation -message $Question -temperature 0.6 -model gpt-4 
+        $response = Invoke-Conversation -message $optimizedQuestion -temperature 0.6 -model gpt-4 
         $Responses += $Response
       }
 
-    Write-Output $Responses
+    $ReasearchPrompt = "You are a researcher tasked with investigating the $iterations response options provided. List the flaws and faulty logic of each answer option. Let's work this out in a step by step way to be sure we have all the errors. The question was, '$Question'. The responses were: "
+    $j = 0 
+   
+   foreach ($response in $Responses) {
+      $ReasearchPrompt += "$j`: $response `n"
+      $j++
+    }
+    
+    Reset-Conversation
+
+    $researchResponse = Invoke-Conversation -message $ReasearchPrompt -temperature 0.6 -model gpt-4 
+
+    Write-Output $researchResponse
+
+    $ResolverPrompt = "$ReasearchPrompt`n$researchResponse`n  You are a resolver tasked with 1: finding which of the $iterations answer options the researcher thought was best. 2: Improving that answer, and 3: Printing the inproved answer in full. Let's work this out in a step by step manner to be sure we have the right answer: `n"
+
+    $ResolverAnswer = Invoke-Conversation -message $ResolverPrompt -temperature .03 -model gpt-4
+
+    Write-Output $ResolverAnswer
+
+
   }
